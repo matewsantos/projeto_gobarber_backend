0529abf9f91b308683454d53eed970cb8c9659fc
Mateus Rodrigues Santos <mateus.rodriguesantos@hotmail.com>
2020-04-27 20:34:21 -0300

    Import do projeto GoBarber

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'aef0eee5ef27c9b7318117a09d9dca89',
+    expiresIn: '1d',
+  },
+};
---
+++ src/service/CreateUsersService.ts
@@ -0,0 +1,38 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface RequestDTO {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUsersService {
+  async execute({ name, email, password }: RequestDTO): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkuserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkuserExists) {
+      throw new Error('Email address already used.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUsersService;
